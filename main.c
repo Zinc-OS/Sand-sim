c9ddf30f28728ce77fc46112c30f80fe9384ccdb
Louis Sarwal <louis.sarwal@gmail.com>
2025-03-10 09:11:23 -0400

    add license text

--- main.c
+++ main.c
@@ -1,3 +1,17 @@
+/*
+This file is part of RomanNumerals.
+
+RomanNumerals is free software: you can redistribute it and/or modify it under the terms
+ of the GNU General Public License as published by the Free Software Foundation, either
+ version 3 of the License, or (at your option) any later version.
+
+RomanNumerals is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with RomanNumerals. 
+If not, see <https://www.gnu.org/licenses/>.*/
+
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <stdint.h>
@@ -8,15 +22,17 @@ struct{
 	int x;
 	int y;
 } mouse = {0,0,0,0,0};
+
 SDL_Window* window;
 SDL_Renderer* renderer;
 SDL_Texture* texture;
-uint32_t*buff;
+uint32_t* buff;
+
 int rnng;
 SDL_Event E;
 int cursorsize=4;
-int width=512, height=512;
-uint32_t color=0xffffffff;
+int width =512, height=512;
+uint32_t color = 0xffffffff;
 long frame=0;
 
 void cleanUp(){
@@ -132,7 +148,7 @@ void updateSand(){
 	}
 }
 
-uint32_t hue(int frame, int time){
+uint32_t hue(int frame, int time, uint8_t value){
 	int x = frame%(time*3);
 	uint32_t color=0x00;
 	if(x<time){
@@ -140,18 +156,21 @@ uint32_t hue(int frame, int time){
 		int c2=0xff*x/time;
 		color+=c1<<16;
 		color+=c2<<8;
+		color+=value;
 	}else if(x<time*2){
 		x-=time;
 		int c1=0xff*(time-x)/time;
 		int c2=0xff*x/time;
 		color+=c1<<8;
 		color+=c2;
+		color+=value<<16;
 	}else{
 		x-=time*2;
 		int c1=0xff*(time-x)/time;
 		int c2=0xff*x/time;
 		color+=c1;
 		color+=c2<<16;
+		color+=value<<8;
 	}
 	return color;
 }
@@ -160,18 +179,19 @@ uint32_t bw(int frame, int time){
 	int x = frame%(time*2);
 	int c=0x00;
 	if(x<time){
-		c=0xf+0xe0*x/time;
+		c=0xe0*x/time;
 	}else{
 		x-=time;
-		c=0xf+0xe0*(time-x)/time;
+		c=0xe0*(time-x)/time;
 	}
+	c+=0x11;
 	c&=0xff;
 	return (c<<16)+(c<<8)+c;
 }
 
 void updateColor(){
-	//color=hue(frame, 200);
-	color=bw(frame, 100);
+	color=hue(frame, 100, 0xff);
+	//color=bw(frame, 100);
 }	
 
 
@@ -201,7 +221,7 @@ int main(int argc, char* argv[]){
 		
 		SDL_Rect cursor={mouse.x-cursorsize, mouse.y-cursorsize, cursorsize*2+1, cursorsize*2+1};
 		SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_NONE);
-		SDL_SetRenderDrawColor(renderer, color&0xff0000>>16, color&0xff00>>8, color&0xff, SDL_ALPHA_OPAQUE);
+		SDL_SetRenderDrawColor(renderer, (color&0xff0000)>>16, (color&0xff00)>>8, color&0xff, SDL_ALPHA_OPAQUE);
 		SDL_RenderFillRect(renderer, &cursor);
         
 		SDL_RenderPresent(renderer);
