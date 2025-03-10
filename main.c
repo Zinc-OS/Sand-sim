4b4e55297d4eced5b1c846e6f01e8c7e76cd3a0d
Louis Sarwal <louis.sarwal@gmail.com>
2025-03-10 14:24:31 -0400

    add spaces

--- main.c
+++ main.c
@@ -56,40 +56,40 @@ void getInputs(){
 			case SDL_MOUSEBUTTONDOWN:
 				switch(E.button.button){
 					case SDL_BUTTON_RIGHT:
-						mouse.right=1;
+						mouse.right = 1;
 						break;
 					case SDL_BUTTON_MIDDLE:
-						mouse.middle=1;
+						mouse.middle = 1;
 						break;
 					case SDL_BUTTON_LEFT:
-						mouse.left=1;
+						mouse.left = 1;
 						break;
 				}
 				break;
 			case SDL_MOUSEBUTTONUP:
 				switch(E.button.button){
 					case SDL_BUTTON_RIGHT:
-						mouse.right=0;
+						mouse.right = 0;
 						break;
 					case SDL_BUTTON_MIDDLE:
-						mouse.middle=0;
+						mouse.middle = 0;
 						break;
 					case SDL_BUTTON_LEFT:
-						mouse.left=0;
+						mouse.left = 0;
 						break;
 				}
 				break;
 			case SDL_WINDOWEVENT:
 				if(E.window.event==SDL_WINDOWEVENT_RESIZED){
-					int oldheight=height;
-					int oldwidth=width;
-					width=E.window.data1;
-					height=E.window.data2;
+					int oldheight = height;
+					int oldwidth = width;
+					width = E.window.data1;
+					height = E.window.data2;
 					uint32_t* buff2=malloc(sizeof(uint32_t)*width*height);
 					for(int i=0;i<oldheight;i++){
 						for(int j=0;j<oldwidth;j++){
 							if(i*width+j<width*height&&i*width+j>=0)
-								buff2[i*width+j]=buff[i*oldwidth+j];
+								buff2[i*width+j] = buff[i*oldwidth+j];
 						}
 					}
 					free(buff);
@@ -108,13 +108,13 @@ void getInputs(){
 
 static inline void setPixel(int pos, uint32_t color){
 	if(pos<width*height&&pos>=0){
-		buff[pos]=color;
+		buff[pos] = color;
 	}
 }
 
 void updateSand(){
 	if(mouse.left){
-		int mid=mouse.x+mouse.y*width;
+		int mid = mouse.x+mouse.y*width;
 		for(int i=-cursorsize;i<=cursorsize;i++){
 			for(int j=-cursorsize;j<=cursorsize;j++){
 				setPixel(mid+i+j*width, color);
@@ -159,22 +159,22 @@ uint32_t hue(int frame, int time, uint8_t value){
 	int x = frame%(time*3);
 	uint32_t color=0x00;
 	if(x<time){
-		int c1=0xff*(time-x)/time;
-		int c2=0xff*x/time;
+		int c1 = 0xff*(time-x)/time;
+		int c2 = 0xff*x/time;
 		color+=c1<<16;
 		color+=c2<<8;
 		color+=value;
 	}else if(x<time*2){
 		x-=time;
-		int c1=0xff*(time-x)/time;
-		int c2=0xff*x/time;
+		int c1 = 0xff*(time-x)/time;
+		int c2 = 0xff*x/time;
 		color+=c1<<8;
 		color+=c2;
 		color+=value<<16;
 	}else{
 		x-=time*2;
-		int c1=0xff*(time-x)/time;
-		int c2=0xff*x/time;
+		int c1 = 0xff*(time-x)/time;
+		int c2 = 0xff*x/time;
 		color+=c1;
 		color+=c2<<16;
 		color+=value<<8;
@@ -186,10 +186,10 @@ uint32_t bw(int frame, int time){
 	int x = frame%(time*2);
 	int c=0x00;
 	if(x<time){
-		c=0xe0*x/time;
+		c = 0xe0*x/time;
 	}else{
 		x-=time;
-		c=0xe0*(time-x)/time;
+		c = 0xe0*(time-x)/time;
 	}
 	c+=0x11;
 	c&=0xff;
@@ -197,8 +197,8 @@ uint32_t bw(int frame, int time){
 }
 
 void updateColor(){
-	color=hue(frame, 100, 0xff);
-	//color=bw(frame, 100);
+	color = hue(frame, 100, 0xff);
+	//color = bw(frame, 100);
 }	
 
 
@@ -212,8 +212,8 @@ int main(int argc, char* argv[]){
 	renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED);
 	texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, width, height);
 	SDL_ShowCursor(0);
-	rnng=1;
-	buff=malloc(width*height*sizeof(uint32_t));
+	rnng = 1;
+	buff = malloc(width*height*sizeof(uint32_t));
 	for(int i=0;i<width*height;i++){
 		setPixel(i, 0x00);
 	}
@@ -223,6 +223,7 @@ int main(int argc, char* argv[]){
 		getInputs();
 		updateColor();
 		updateSand();
+		
 		SDL_UpdateTexture(texture, NULL, buff, width*sizeof(uint32_t));
 		SDL_RenderCopy(renderer, texture, NULL, NULL);
 		
